e62c7f9d11d0d9ee53ac4dbc372b5d1de504b76d
Mathieu Ibersien <mat98-76@live.fr>
2021-10-29 09:36:29 +0200

    first commit working horizontal slide

---
+++ index.js
@@ -0,0 +1,14 @@
+import LocomotiveScroll from 'locomotive-scroll';
+
+
+
+
+const scroller = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true
+});
+
+
+/* const target = document.querySelector('#js-target');
+
+scroll.scrollTo(target); */
