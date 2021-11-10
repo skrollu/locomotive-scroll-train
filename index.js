e8919b59643ee6c39a23ac722d5097666de66a16
Mathieu Ibersien <mat98-76@live.fr>
2021-11-10 23:14:35 +0100

    test de delay pas concluant...

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import LocomotiveScroll from 'locomotive-scroll';
 const scroll = new LocomotiveScroll({
   el: document.querySelector('[data-scroll-container]'),
   smooth: true,
-  multiplier:0.9
+  repeat: true,
+  getSpeed: !1,
+//  multiplier:0.9
   //lerp: 1 // +/- fps / refresh images
 });
 
@@ -15,8 +17,10 @@ scroll.scrollTo(target); */
 scroll.on('call', func => {
   if(func === "slowDown")
     slowDown();
+  else if(func === "delay")
+    console.log('delay')
 });
 
 const slowDown = () => {
-  console.log("coucou")
+  console.log("coucou");
 }
